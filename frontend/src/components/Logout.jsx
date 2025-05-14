626f006ad02eece52921f90b88edaa216a5c1e96
sofisando <sofisandobal9@gmail.com>
2025-05-13 23:07:48 -0300

    logout funcional again

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -1,20 +1,29 @@
 import { useNavigate } from "react-router-dom";
-import { resetUser, UserKey } from "../redux/userSlice"
-import { clearLocalStorage } from "../utilities/localStorage.utility"
+import { resetUser } from "../redux/userSlice";
 import { PublicRoutes } from "../models/routes";
 import { useDispatch } from "react-redux";
 
 function Logout() {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const logOut = () => {
-        clearLocalStorage(UserKey)
-        dispatch(resetUser());
-        navigate(PublicRoutes.LOGIN, {replace: true});
-    };
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const logOut = async () => {
+    try {
+      await fetch('http://localhost:5000/auth/logout', {
+        method: 'POST',
+        credentials: 'include'
+      });
+    } catch (error) {
+      console.warn("Error cerrando sesión:", error);
+    }
+
+    dispatch(resetUser());
+    navigate(PublicRoutes.LOGIN, { replace: true });
+  };
 
   return (
     <button className="button-pretty" onClick={logOut}>Log Out</button>
-  )
+  );
 }
-export default Logout
+
+export default Logout;
